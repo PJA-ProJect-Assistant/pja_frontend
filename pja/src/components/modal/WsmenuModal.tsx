1428ffdbb8210e8ba7306a2fd83a24c65800f28a
coq-j01 <chaebinesxo@gmail.com>
2025-05-29 15:25:14 +0900

    feat : navigate 추가

--- pja/src/components/modal/WsmenuModal.tsx
+++ pja/src/components/modal/WsmenuModal.tsx
@@ -45,3 +45,25 @@ export function WscompleteModal({ onClose }: IsClose) {
     </Dialog.Root>
   );
 }
+
+export function StepRestrictionModal({ onClose }: IsClose) {
+  // 수정,삭제,완료 등 권한 없을 때 띄우는 모달
+  return (
+    <Dialog.Root open={true} onOpenChange={onClose}>
+      <Dialog.Portal>
+        <Dialog.Overlay className="modal-overlay" />
+        <Dialog.Content className="modal-content">
+          <Dialog.Title className="modal-title">
+            이동이 불가능합니다
+          </Dialog.Title>
+          <Dialog.Description className="modal-description">
+            이전 단계를 모두 완료한 후 이동 가능합니다
+          </Dialog.Description>
+          <Dialog.Close asChild>
+            <button className="modal-close">확인</button>
+          </Dialog.Close>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+}
--- pja/src/components/sidebar/ProgressStep.tsx
+++ pja/src/components/sidebar/ProgressStep.tsx
@@ -1,10 +1,36 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import type { RootState } from "../../store/store";
 import "./ProgressStep.css"
+import { useNavigate } from "react-router-dom";
+import { StepRestrictionModal } from "../modal/WsmenuModal";
+import { useState } from "react";
+import { WscompleteModal } from "../modal/WsmenuModal";
+import { setSelectedWS } from "../../store/workspaceSlice";
+
 export default function ProgressStep() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
     const selectedWS = useSelector(
         (state: RootState) => state.workspace.selectedWS
     );
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
+    const [completeModalOpen, setCompleteModalOpen] = useState<boolean>(false);
+    const handleStepClick = (step: number) => {
+        if (step === 5) {
+            if (selectedWS?.progress_step === step) {
+                dispatch(setSelectedWS({ ...selectedWS, progress_step: 6 }))
+                navigate(`/ws/${selectedWS?.workspace_id}/step/${step}`);
+            }
+            else if (selectedWS?.progress_step == 6) {
+
+            }
+            else setCompleteModalOpen(true);
+        }
+        else if (typeof selectedWS?.progress_step === "number" && step <= selectedWS.progress_step) {
+            navigate(`/ws/${selectedWS?.workspace_id}/step/${step}`);
+        }
+        else setModalOpen(true);
+    };
     console.log({ selectedWS })
     const steps = [
         "아이디어 결정",
@@ -15,31 +41,37 @@ export default function ProgressStep() {
         "프로젝트 완료",
     ];
     return (
-        <div className="progress-container">
-            <h3>프로젝트 진행 상황</h3>
-            <div className="progressbar-container">
-                <div className="bar-container">
-                    {steps.map((_, index) => {
-                        let barClass = "bar";
-                        if (index === 0) barClass += " top";
-                        if (index === steps.length - 1) barClass += " bottom";
-                        if (index < (selectedWS?.progress_step ?? 0)) {
-                            barClass += " filled";
-                        } else if (index === (selectedWS?.progress_step ?? 0)) {
-                            barClass += " gradient";
-                        }
+        <>
+            <div className="progress-container">
+                <h3>프로젝트 진행 상황</h3>
+                <div className="progressbar-container">
+                    <div className="bar-container">
+                        {steps.map((_, index) => {
+                            let barClass = "bar";
+                            if (index === 0) barClass += " top";
+                            if (index === steps.length - 1) barClass += " bottom";
+                            if (index < (selectedWS?.progress_step ?? 0)) {
+                                barClass += " filled";
+                            } else if (index === (selectedWS?.progress_step ?? 0)) {
+                                barClass += " gradient";
+                            }
 
-                        return <div key={index} className={barClass}></div>;
-                    })}
-                </div>
-                <div className="labels">
-                    {steps.map((label, index) => (
-                        <div key={index} className="label">
-                            {label}
-                        </div>
-                    ))}
+                            return <div key={index} className={barClass}></div>;
+                        })}
+                    </div>
+                    <div className="labels">
+                        {steps.map((label, index) => (
+                            <div key={index} className="label" onClick={() => {
+                                handleStepClick(index);
+                            }}>
+                                {label}
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
+            {modalOpen && <StepRestrictionModal onClose={() => setModalOpen(false)} />}
+            {completeModalOpen && <WscompleteModal onClose={() => setCompleteModalOpen(false)} />}
+        </>
     );
 }
