fd1e5b99758da27ea2d853e75ce997212168d352
coq-j01 <chaebinesxo@gmail.com>
2025-05-31 16:07:16 +0900

    feat : 주요 기능 box 추가

--- pja/src/pages/workspacepage/developmentpage/listpage/ListPage.tsx
+++ pja/src/pages/workspacepage/developmentpage/listpage/ListPage.tsx
@@ -1,11 +1,13 @@
 import "./ListPage.css"
 import CategoryProgress from "./CategoryProgress"
+import Featurelist from "./Featurelist"
 
 export default function ListPage() {
     return (
         <div className="list-container">
             <div className="list-info" >
                 <CategoryProgress />
+                <Featurelist />
             </div>
             <div className="list-table-box">
 
