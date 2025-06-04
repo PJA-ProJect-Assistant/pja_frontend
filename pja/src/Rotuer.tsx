8e84ef2438bbd1969529573adf55be5e46b57f55
coq-j01 <chaebinesxo@gmail.com>
2025-06-04 17:40:21 +0900

    feat : actionpost 생성

--- pja/src/Rotuer.tsx
+++ pja/src/Rotuer.tsx
@@ -7,28 +7,27 @@ import FindPwPage from "./pages/loginpage/findpw/FindPwPage";
 import SignupPage from "./pages/signuppage/SignupPage";
 import AddWSPage from "./pages/workspacepage/addworkspacepage/AddWSPage";
 import MainWSPage from "./pages/workspacepage/mainworkspacepage/MainWSPage";
+import ActionPostPage from "./pages/workspacepage/developmentpage/postpage/ActionPostPage";
 
 const Router = () => {
   return (
     <Routes>
       {/* 기본 경로에서 로그인으로 리다이렉트 */}
-      {/* 로그인해서 들어가게 하는거는 백이랑 연결하고 일단 들어가면 바로 홈 나오게 할게여ㅕㅕ */}
+      {/* 로그인해서 들어가게 하는거는 백이랑 연결하고 일단 들어가면 바로 홈 나오게 할게여 */}
       <Route path="/" element={<Navigate to="/main" replace />} />
 
       {/* 공개 라우트 */}
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/main" element={<MainPage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/find-id" element={<FindIdPage />} />
       <Route path="/find-pw" element={<FindPwPage />} />
       {/* 인증 필요 라우트 */}
       {/* <Route element={<PrivateRoute />}*/}
       {/*</Route> */}
-
+      <Route path="/main" element={<MainPage />} />
       <Route path="/addws" element={<AddWSPage />} />
-
       <Route path="/ws/:wsid/step/:stepNumber" element={<MainWSPage />} />
-
+      <Route path="/ws/:wsid/action/:acId" element={<ActionPostPage />} />
       {/*로그인 성공 후 리디렉션 처리용 페이지 */}
       {/* <Route path="/oauth2/success" element={<OAuth2Success />} /> */}
     </Routes>
