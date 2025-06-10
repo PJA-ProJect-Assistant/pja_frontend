6e87c6dbabe7afc33a3f4bcdccd7b661f1d483cd
coq-j01 <chaebinesxo@gmail.com>
2025-06-10 13:51:44 +0900

    refactor : api명세서 페이지 리팩토링

--- pja/src/Rotuer.tsx
+++ pja/src/Rotuer.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import MainPage from "./pages/mainpage/MainPage";
-import ApiPage from "./pages/mainpage/ApiPage";
 import LoginPage from "./pages/loginpage/LoginPage";
 import OAuth2Success from "./pages/loginpage/success/OAuth2SuccessPage";
 import FindIdPage from "./pages/loginpage/findid/FindIdPage";
@@ -13,35 +12,34 @@ import WorkspaceSettingPage from "./pages/workspacesettingpage/WorkspaceSettingP
 import ActionPostPage from "./pages/workspacepage/developmentpage/postpage/ActionPostPage";
 
 const Router = () => {
-      return (
-            <Routes>
-                  {/* 기본 경로에서 로그인으로 리다이렉트 */}
-                  {/* 로그인해서 들어가게 하는거는 백이랑 연결하고 일단 들어가면 바로 홈 나오게 할게여 */}
-                  <Route path="/" element={<Navigate to="/main" replace />} />
+  return (
+    <Routes>
+      {/* 기본 경로에서 로그인으로 리다이렉트 */}
+      {/* 로그인해서 들어가게 하는거는 백이랑 연결하고 일단 들어가면 바로 홈 나오게 할게여 */}
+      <Route path="/" element={<Navigate to="/main" replace />} />
 
-                  {/* 공개 라우트 */}
-                  <Route path="/login" element={<LoginPage />} />
-                  <Route path="/main" element={<MainPage />} />
-                  <Route path="/signup" element={<SignupPage />} />
-                  <Route path="/find-id" element={<FindIdPage />} />
-                  <Route path="/find-pw" element={<FindPwPage />} />
-                  {/* 인증 필요 라우트 */}
-                  {/* <Route element={<PrivateRoute />}*/}
-                  {/*</Route> */}
-                  <Route path="/main" element={<MainPage />} />
-                  <Route path="/apipage" element={<ApiPage />} />
-                  <Route path="/addws" element={<AddWSPage />} />
-                  <Route path="/ws/:wsid/step/:stepNumber" element={<MainWSPage />} />
-                  <Route path="/ws/:wsid/action/:acId" element={<ActionPostPage />} />
-                  {/*로그인 성공 후 리디렉션 처리용 페이지 */}
-                  <Route path="/oauth2/success" element={<OAuth2Success />} />
+      {/* 공개 라우트 */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/main" element={<MainPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/find-id" element={<FindIdPage />} />
+      <Route path="/find-pw" element={<FindPwPage />} />
+      {/* 인증 필요 라우트 */}
+      {/* <Route element={<PrivateRoute />}*/}
+      {/*</Route> */}
+      <Route path="/main" element={<MainPage />} />
+      <Route path="/addws" element={<AddWSPage />} />
+      <Route path="/ws/:wsid/step/:stepNumber" element={<MainWSPage />} />
+      <Route path="/ws/:wsid/action/:acId" element={<ActionPostPage />} />
+      {/*로그인 성공 후 리디렉션 처리용 페이지 */}
+      <Route path="/oauth2/success" element={<OAuth2Success />} />
 
-                  {/*계정설정 페이지*/}
-                  <Route path="/account-settings" element={<AccountSettingPage />} />
+      {/*계정설정 페이지*/}
+      <Route path="/account-settings" element={<AccountSettingPage />} />
 
-                  <Route path="/workspace-setting" element={<WorkspaceSettingPage />} />
-            </Routes >
-      );
+      <Route path="/workspace-setting" element={<WorkspaceSettingPage />} />
+    </Routes>
+  );
 };
 
 export default Router;
---
+++ pja/src/pages/workspacepage/apispecpage/ApiPage.tsx
@@ -0,0 +1,151 @@
+import { useState } from "react";
+import "./ApiPage.css";
+import plusIcon from "../../../assets/img/plus.png";
+import minusIcon from "../../../assets/img/minus.png";
+import { WSHeader } from "../../../components/header/WSHeader";
+
+const ApiPage = () => {
+  //타입 명시
+  type ApiRow = {
+    id: number;
+    category: string;
+    feature: string;
+    auth: string;
+    method: string;
+    url: string;
+    io: string;
+  };
+
+  /*테이블 데이터를 배열 형태로 저장*/
+  const [rows, setRows] = useState<ApiRow[]>([
+    {
+      id: Date.now(), // 고유 id 추가
+      category: "Category",
+      feature: "Feature",
+      auth: "Auth",
+      method: "Method",
+      url: "URL",
+      io: "request/response",
+    },
+  ]);
+
+  // 행 추가
+  const handleAddRow = () => {
+    const newRow = {
+      id: Date.now(), // 고유한 id 부여
+      category: "",
+      feature: "",
+      auth: "",
+      method: "",
+      url: "",
+      io: "",
+    };
+    setRows([...rows, newRow]);
+  };
+
+  // 해당 행 삭제
+  // row.id !== id 로 해당 id만 제거
+  const handleDeleteRow = (id: number) => {
+    if (rows.length === 1) {
+      alert("최소 1개의 행은 있어야 합니다!");
+      return;
+    }
+    setRows(rows.filter((row) => row.id !== id));
+  };
+
+  //수정하기 버튼 클릭 시 input으로 변경
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  const toggleEditMode = () => {
+    setIsEditMode(!isEditMode);
+  };
+  //셀 내용 변경
+  const handleChange = (
+    id: number,
+    field: keyof Omit<ApiRow, "id">,
+    value: string
+  ) => {
+    const updateRows = rows.map((row) =>
+      row.id === id ? { ...row, [field]: value } : row
+    );
+    setRows(updateRows);
+  };
+
+  return (
+    <div>
+      <WSHeader title="API 명세서" />
+      <div className="api-main">
+        <div className="api-header">
+          <div className="api-alert">
+            ✨아이디어와 명세서를 분석하여 API 추천을 해드려요
+          </div>
+          <div className="api-button-group">
+            <button className="api-edit-button" onClick={toggleEditMode}>
+              {isEditMode ? "수정완료" : "수정하기"}
+            </button>
+          </div>
+        </div>
+
+        <table className="api-table">
+          <thead>
+            <tr>
+              <th className="api-table-title">분류</th>
+              <th className="api-table-title">기능</th>
+              <th className="api-table-title">인증방식</th>
+              <th className="api-table-title">http 매서드</th>
+              <th className="api-table-title">api 경로</th>
+              <th className="api-table-title">요청/응답</th>
+
+              {/* ✅ 삭제 버튼 칼럼 */}
+            </tr>
+          </thead>
+          <tbody>
+            {rows.map((row) => (
+              <tr key={row.id}>
+                {["category", "feature", "auth", "method", "url", "io"].map(
+                  (field) => (
+                    <td className="api-table-content" key={field}>
+                      {isEditMode ? (
+                        <input
+                          type="text"
+                          value={row[field as keyof Omit<ApiRow, "id">]}
+                          onChange={(e) =>
+                            handleChange(
+                              row.id,
+                              field as keyof Omit<ApiRow, "id">,
+                              e.target.value
+                            )
+                          }
+                          className="api-edit-input"
+                        />
+                      ) : (
+                        row[field as keyof Omit<ApiRow, "id">]
+                      )}
+                    </td>
+                  )
+                )}
+
+                <td className="api-table-content no-border">
+                  <button
+                    className="api-delete-row-button"
+                    onClick={() => handleDeleteRow(row.id)}
+                  >
+                    <img src={minusIcon} alt="삭제" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <div className="api-button-group">
+          <button className="api-add-button" onClick={handleAddRow}>
+            <img src={plusIcon} alt="plusIcon" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ApiPage;
