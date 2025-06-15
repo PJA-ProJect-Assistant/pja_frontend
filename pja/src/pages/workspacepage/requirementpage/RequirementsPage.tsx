d83d0d96969c600656addd9214564da752fb2cee
coq-j01 <chaebinesxo@gmail.com>
2025-06-15 18:58:52 +0900

    feat: 요구사항 명세서 title 생성

--- pja/src/pages/workspacepage/requirementpage/RequirementsPage.tsx
+++ pja/src/pages/workspacepage/requirementpage/RequirementsPage.tsx
@@ -1,9 +1,16 @@
 import { WSHeader } from "../../../components/header/WSHeader";
+import "./RequirementsPage.css"
 
 export default function RequirementsPage() {
     return (
         <>
             <WSHeader title="요구사항 명세서" />
+            <div className="require-container">
+                <div className="require-title">
+                    <p>✨기능/성능 요구사항 3개 이상 입력하면 AI 추천이 가능해요</p>
+                    <div className="require-aibtn ">AI 추천받기</div>
+                </div>
+            </div>
         </>
     )
 }
