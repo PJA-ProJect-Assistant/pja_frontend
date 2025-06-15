96f04597d0e44504fdbae51380af5f1b5850ca47
coq-j01 <chaebinesxo@gmail.com>
2025-06-15 17:30:58 +0900

    feat : 아이디어 입력 페이지 api 연결

--- pja/src/pages/workspacepage/ideapage/IdeaPage.tsx
+++ pja/src/pages/workspacepage/ideapage/IdeaPage.tsx
@@ -1,14 +1,8 @@
-import { useSelector } from "react-redux";
-import type { RootState } from "../../../store/store";
-import { useEffect, useState } from "react";
 import { WSHeader } from "../../../components/header/WSHeader";
 import ProjectForm from "./ProjectForm";
 import "./IdeaPage.css";
 
 export default function IdeaPage() {
-  const selectedWS = useSelector(
-    (state: RootState) => state.workspace.selectedWS
-  );
   return (
     <div className="ideapage-container">
       <WSHeader title="아이디어 요약" />
