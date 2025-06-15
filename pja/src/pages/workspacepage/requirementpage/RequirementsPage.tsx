a8cda8d2a24a19d74612372b340be1f20d1e7eb4
coq-j01 <chaebinesxo@gmail.com>
2025-06-15 23:21:25 +0900

    feat : 요구사항 명세서 api 생성
    제대로 작동되는지 확인 못 함

--- pja/src/pages/workspacepage/requirementpage/RequirementsPage.tsx
+++ pja/src/pages/workspacepage/requirementpage/RequirementsPage.tsx
@@ -1,7 +1,72 @@
 import { WSHeader } from "../../../components/header/WSHeader";
 import "./RequirementsPage.css"
+import type { setrequire, getrequire } from "../../../types/requirement";
+import { useEffect, useState } from "react";
+import { getrequirement, inputrequirement, Requirementgenerate } from "../../../services/requirementApi";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "../../../store/store";
 
 export default function RequirementsPage() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const selectedWS = useSelector(
+        (state: RootState) => state.workspace.selectedWS
+    );
+    const [requireDone, setRequireDone] = useState<boolean>(false);
+    const [requirements, setRequirements] = useState<getrequire[]>([]);
+    useEffect(() => {
+        const getRequire = async () => {
+            if (selectedWS?.workspaceId) {
+                const response = await getrequirement(selectedWS?.workspaceId);
+                console.log("요구사항 조회 결과 :", response);
+                if (response.data) {
+                    setRequirements(response.data); // undefined가 아닐 때만 설정
+                }
+            }
+        }
+        getRequire();
+    }, [])
+    const handleAddFunction = async () => {
+        try {
+            if (selectedWS?.workspaceId) {
+                const response = await inputrequirement(selectedWS?.workspaceId, "FUNCTIONAL");
+                console.log("function 생성 결과 :", response);
+            }
+        }
+        catch (err) {
+            console.log("function 생성 실패", err);
+
+        }
+    }
+    const handleAddPerformance = async () => {
+        try {
+            if (selectedWS?.workspaceId) {
+                const response = await inputrequirement(selectedWS?.workspaceId, "PERFORMANCE");
+                console.log("performance 생성 결과 :", response);
+            }
+        }
+        catch (err) {
+            console.log("performance 생성 실패", err);
+
+        }
+    }
+    const handleAiRequirements = async () => {
+        try {
+            if (selectedWS?.workspaceId) {
+                const setrequirement: setrequire[] = requirements.map(({ requirementType, content }) => ({
+                    requirementType,
+                    content
+                }));
+                const response = await Requirementgenerate(selectedWS.workspaceId, setrequirement);
+                console.log("요구사항 저장 성공:", response);
+                alert("요구사항이 성공적으로 저장되었습니다!");
+            }
+        } catch (err) {
+            console.error("요구사항 저장 실패", err);
+            alert("요구사항 저장 중 오류가 발생했습니다.");
+        }
+    };
     return (
         <>
             <WSHeader title="요구사항 명세서" />
@@ -9,6 +74,22 @@ export default function RequirementsPage() {
                 <div className="require-title">
                     <p>✨기능/성능 요구사항 3개 이상 입력하면 AI 추천이 가능해요</p>
                     <div className="require-aibtn ">AI 추천받기</div>
+                </div>
+                <div className="require-content">
+                    <div className="require-content-box ">
+                        {!requireDone && <button className="require-add-button" onClick={handleAddFunction}>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M444-444H240v-72h204v-204h72v204h204v72H516v204h-72v-204Z" /></svg>
+                            <p>요구사항 추가</p>
+                        </button>
+                        }
+                    </div>
+                    <div className="require-content-box ">
+                        {!requireDone && <button className="require-add-button" onClick={handleAddPerformance}>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M444-444H240v-72h204v-204h72v204h204v72H516v204h-72v-204Z" /></svg>
+                            <p>요구사항 추가</p>
+                        </button>
+                        }
+                    </div>
                 </div>
             </div>
         </>
---
+++ pja/src/services/requirementApi.ts
@@ -0,0 +1,78 @@
+import api from "../lib/axios";
+import type { ApiResponse } from "../types/common";
+import type { getrequire, setrequire } from "../types/requirement";
+//요구사항 명세서 api
+
+//요구사항 생성
+export const inputrequirement = async (workspaceId: number, requirementType: string): Promise<ApiResponse<getrequire>> => {
+    try {
+        console.log("요구사항 생성 api");
+        const response = await api.post(`/workspaces/${workspaceId}/requirements`, {
+            requirementType,
+            content: "",
+        });
+        console.log("요구사항 생성 :", response);
+        return response.data;
+    } catch (error: any) {
+        console.error("🔴 [inputrequirement] 요구사항 생성 실패:", error);
+
+        if (error.response) {
+            console.error("응답 상태코드:", error.response.status);
+            console.error("서버 status:", error.response.data?.status);
+            console.error("서버 message:", error.response.data?.message);
+        } else if (error.request) {
+            console.error("요청은 보냈지만 응답 없음:", error.request);
+        } else {
+            console.error("요청 설정 중 에러 발생:", error.message);
+        }
+        throw error;
+    }
+}
+
+//요구사항 조회
+export const getrequirement = async (workspaceId: number): Promise<ApiResponse<getrequire[]>> => {
+    try {
+        console.log("요구사항 조회 api");
+        const response = await api.get(`/workspaces/${workspaceId}/requirements`);
+        console.log("요구사항 조회 :", response);
+        return response.data;
+    } catch (error: any) {
+        console.error("🔴 [getrequirement] 요구사항 조회 실패:", error);
+
+        if (error.response) {
+            console.error("응답 상태코드:", error.response.status);
+            console.error("서버 status:", error.response.data?.status);
+            console.error("서버 message:", error.response.data?.message);
+        } else if (error.request) {
+            console.error("요청은 보냈지만 응답 없음:", error.request);
+        } else {
+            console.error("요청 설정 중 에러 발생:", error.message);
+        }
+        throw error;
+    }
+}
+
+//요구사항 명세서 ai추천
+export const Requirementgenerate = async (workspaceId: number, requirements: setrequire[]): Promise<ApiResponse<getrequire[]>> => {
+    try {
+        console.log("요구사항 ai추천 api");
+        const response = await api.post(`/workspaces/${workspaceId}/requirements/generate`, {
+            requirements,
+        });
+        console.log("요구사항 ai추천 :", response);
+        return response.data;
+    } catch (error: any) {
+        console.error("🔴 [Requirementgenerate] 요구사항 ai추천 실패:", error);
+
+        if (error.response) {
+            console.error("응답 상태코드:", error.response.status);
+            console.error("서버 status:", error.response.data?.status);
+            console.error("서버 message:", error.response.data?.message);
+        } else if (error.request) {
+            console.error("요청은 보냈지만 응답 없음:", error.request);
+        } else {
+            console.error("요청 설정 중 에러 발생:", error.message);
+        }
+        throw error;
+    }
+}
