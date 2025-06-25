00681719d950698d55d5a8d0a964a28aefe4f853
coq-j01 <chaebinesxo@gmail.com>
2025-06-25 12:59:32 +0900

    feat : userRole 반환, 접근 제한 추가

--- pja/src/pages/workspacepage/mainworkspacepage/MainWSPage.tsx
+++ pja/src/pages/workspacepage/mainworkspacepage/MainWSPage.tsx
@@ -13,6 +13,8 @@ import ApiPage from "../apispecpage/ApiPage";
 import DevelopmentPage from "../developmentpage/DevelopmentPage";
 import ProjectSummaryPage from "../projectsummarypage/ProjectSummaryPage";
 import { ReactFlowProvider } from "reactflow";
+import { getUserRole } from "../../../services/userApi";
+import { setUserRole } from "../../../store/userSlice";
 
 export default function MainWSPage() {
   const { wsid, stepId } = useParams<{
@@ -22,6 +24,8 @@ export default function MainWSPage() {
   const dispatch = useDispatch(); //redux에 값 저장하는 함수 필요
   const [sidebarOpen, setSidebarOpen] = useState(true);
   const [showIcon, setShowIcon] = useState(false);
+  const [userRole, serUserRole] = useState<string | null>(null);
+  const [wsPublic, setWsPublic] = useState<boolean>(true);
 
   useEffect(() => {
     const getws = async () => {
@@ -31,12 +35,19 @@ export default function MainWSPage() {
         //redux저장
         if (response.data) {
           dispatch(setSelectedWS(response.data));
+          setWsPublic(response.data.isPublic);
         }
       } catch (err) {
         console.log("getworkspace 실패 : ", err);
       }
     };
+    const getrole = async () => {
+      const response = await getUserRole(Number(wsid));
+      console.log("사용자 역할 : ", response.data?.role);
+      dispatch(setUserRole(response.data?.role ?? null));
+    };
     getws();
+    getrole();
   }, [wsid]);
   const renderStepComponent = () => {
     switch (stepId) {
@@ -62,7 +73,12 @@ export default function MainWSPage() {
     }
   };
 
-  return (
+  return !wsPublic && userRole === null ? (
+    <div className="NoEnty-mainws">
+      <p>해당 워크스페이스를 조회할 권한이 없습니다</p>
+      <button onClick={() => history.back()}>돌아가기</button>
+    </div>
+  ) : (
     <div className="mainws-container">
       <AnimatePresence
         onExitComplete={() => {
