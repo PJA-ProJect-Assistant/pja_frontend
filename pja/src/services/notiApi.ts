b7762da31aa64468c275cb8d75bfea66b39da3d8
Umin73 <97716008+Umin73@users.noreply.github.com>
2025-06-29 17:51:43 +0900

    알림 개별 읽음 처리 및 개별 삭제 api 연결

--- pja/src/services/notiApi.ts
+++ pja/src/services/notiApi.ts
@@ -50,7 +50,32 @@ export const readAllNotifications = async (
     }
   }
 
-  // 알림 전체 읽음 처리
+  // 알림 개별 읽음 처리
+export const readNotification = async (
+  workspaceId: number,
+  notiId: number
+) : Promise<void> => {
+  const accessToken = localStorage.getItem("accessToken");
+  try {
+    const response = await api.patch(`/workspaces/${workspaceId}/noti/${notiId}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (response.data.status !== "success") {
+      throw new Error(response.data.message || "알림 개별 읽음 처리 실패");
+    }
+    
+    console.log("개별 알림을 읽음 처리 완료");
+
+    } catch(error) {
+      console.error("알림 개별 읽음 처리 실패:", error);
+      throw error;
+    }
+  }
+
+// 알림 전체 삭제 처리
 export const deleteAllNotifications = async (
   workspaceId: number
 ) : Promise<void> => {
@@ -70,6 +95,31 @@ export const deleteAllNotifications = async (
 
     } catch(error) {
       console.error("알림 전체 삭제 실패:", error);
+      throw error;
+    }
+  }
+
+  // 알림 개별 삭제 처리
+export const deleteNotification = async (
+  workspaceId: number,
+  notiId: number
+) : Promise<void> => {
+  const accessToken = localStorage.getItem("accessToken");
+  try {
+    const response = await api.delete(`/workspaces/${workspaceId}/noti/${notiId}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (response.data.status !== "success") {
+      throw new Error(response.data.message || "알림 개별 삭제 실패");
+    }
+    
+    console.log("개별 알림을 삭제 완료");
+
+    } catch(error) {
+      console.error("알림 개별 삭제 실패:", error);
       throw error;
     }
   }
