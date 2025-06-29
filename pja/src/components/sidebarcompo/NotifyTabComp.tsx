13337e8e7ac853cc1a4837ec938d1e04c9893f75
coq-j01 <chaebinesxo@gmail.com>
2025-06-29 17:56:33 +0900

    remove : 미사용 아이콘 제거

--- pja/src/components/sidebarcompo/NotifyTabComp.tsx
+++ pja/src/components/sidebarcompo/NotifyTabComp.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./NotifyTabComp.css";
-import signbellIcon from "../../assets/img/signbell.png";
 import NotifyItem from "./NotifyItem";
 import { useSelector } from "react-redux";
 import type { RootState } from "../../store/store";
@@ -19,7 +18,7 @@ const NotifyTabComp = ({ notifications, setNotifications }: NotifyTabCompProps) 
   );
   const workspaceId = selectedWS?.workspaceId;
   console.log("workspaceId 는 ", workspaceId);
-  
+
   useEffect(() => {
     if (!workspaceId) return;
 
@@ -86,23 +85,23 @@ const NotifyTabComp = ({ notifications, setNotifications }: NotifyTabCompProps) 
     <>
       <div className="notify-list-container">
         <svg
-        className="notify-bell-icon"
-            xmlns="http://www.w3.org/2000/svg"
-            height="43px"
-            viewBox="0 -960 960 960"
-            width="43px"
-            fill="#000000"
-          >
-            <path d="M160-200v-80h80v-280q0-83 50-147.5T420-792v-28q0-25 17.5-42.5T480-880q25 0 42.5 17.5T540-820v28q80 20 130 84.5T720-560v280h80v80H160Zm320-300Zm0 420q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80ZM320-280h320v-280q0-66-47-113t-113-47q-66 0-113 47t-47 113v280Z" />
+          className="notify-bell-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          height="43px"
+          viewBox="0 -960 960 960"
+          width="43px"
+          fill="#000000"
+        >
+          <path d="M160-200v-80h80v-280q0-83 50-147.5T420-792v-28q0-25 17.5-42.5T480-880q25 0 42.5 17.5T540-820v28q80 20 130 84.5T720-560v280h80v80H160Zm320-300Zm0 420q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80ZM320-280h320v-280q0-66-47-113t-113-47q-66 0-113 47t-47 113v280Z" />
         </svg>
         {notifications.map((noti) => (
-          <NotifyItem 
-            key={noti.notificationId} 
-            message={noti.message} 
+          <NotifyItem
+            key={noti.notificationId}
+            message={noti.message}
             noti={noti}
             onRead={handleRead}
             onDelete={handleDelete}
-            />
+          />
         ))}
       </div>
     </>
